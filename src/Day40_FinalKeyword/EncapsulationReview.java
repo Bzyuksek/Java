245f0985cf945d2db59fdbe65a62810961a7f76d
Bekir <bzyuksek@gmail.com>
2022-12-24 12:12:03 +0300

    Commiting files

---
+++ src/Day40_FinalKeyword/EncapsulationReview.java
@@ -0,0 +1,36 @@
+package Day40_FinalKeyword;
+
+
+import Day39_Recap.shapeTask.Circle;
+import Day39_Recap.shapeTask.Square;
+
+public class EncapsulationReview {
+
+    private Circle circle;
+
+    private Square square;
+
+    public Circle getCircle(){
+        return circle;
+    }
+
+    public  void setCircle(Circle circle){
+
+        if(circle.getRadius() < 5){
+            return;
+        }
+
+        this.circle = circle;
+    }
+
+
+    public Square getSquare() {
+        return square;
+    }
+
+    public void setSquare(Square square) {
+        this.square = square;
+    }
+
+
+}
